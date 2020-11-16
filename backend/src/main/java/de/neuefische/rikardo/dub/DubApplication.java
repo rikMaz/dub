0d8e74a4660b90c201708f97bec2e6274d334840
Rikardo <rikardo@marenzzi.de>
2020-11-16 10:57:58 +0100

    Init spring server

---
+++ backend/src/main/java/de/neuefische/rikardo/dub/DubApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.rikardo.dub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubApplication.class, args);
+	}
+
+}
