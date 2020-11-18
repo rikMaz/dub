17a29f5e3e17f5e12d82c91ceeee62260a3bf8cf
Rikardo <rikardo@marenzzi.de>
2020-11-18 14:11:41 +0100

    axios server connection is working, getting list of movies after search by name

---
+++ frontend/src/commons/Movie.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Movie({movie}){
+  return(
+    <>
+      <div>{movie.title}</div>
+    </>
+  )
+}
--- frontend/src/searchResultPage/MovieList.js
+++ frontend/src/searchResultPage/MovieList.js
@@ -1,15 +1,22 @@
-import React from "react";
-import { useHistory } from 'react-router-dom';
+import React, { useContext } from 'react';
 import SearchContext from "../context/SearchContext";
+import Movie from "../commons/Movie";
 
 
 export default function MovieList() {
-  const {movies} = useContext(SearchContext);
-  const history = useHistory();
+  const { movies } = useContext(SearchContext);
 
   return(
     <>
       <div>MovieList</div>
+      <ul>
+        {movies?.map((movie) =>
+          <li key={movie.id}>
+            <Movie movie={movie}/>
+
+          </li>
+        )}
+      </ul>
     </>
   )
 }
