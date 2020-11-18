e531d877861d6c3eb657e4ab49a3d076a020c2bb
Rikardo <rikardo@marenzzi.de>
2020-11-18 16:59:09 +0100

    add axios method getMovieDetailsById, add feature user click on movie image to get movie details

--- frontend/src/movieDetailsPage/MovieDetailsPage.js
+++ frontend/src/movieDetailsPage/MovieDetailsPage.js
@@ -1,11 +1,26 @@
-import React from "react";
-
+import React, {useContext, useEffect, useState} from 'react';
+import SearchContext from "../context/SearchContext";
+import {useHistory} from "react-router-dom";
 
 export default function MovieDetailsPage() {
+  const [imageUrl,setImageUrl] = useState("");
+  const imageUrlBasis = "https://image.tmdb.org/t/p/w154/"
+  const history = useHistory();
+  const {movie} = useContext(SearchContext);
+
+  useEffect(() => {
+    setImageUrl(imageUrlBasis + movie?.poster_path);
+  },[movie]);
 
   return (
     <>
-      <div>MovieDetailsPage</div>
+      <div>{movie?.title}</div>
+      <img alt="MovieImage" src={imageUrl}/>
+      <button onClick={onCancel}>Cancel</button>
     </>
   )
+
+  function onCancel() {
+    history.goBack();
+  }
 }
