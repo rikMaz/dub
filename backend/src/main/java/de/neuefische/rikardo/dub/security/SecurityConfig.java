012c52f45c82d30fcb4a1b5ea5684eec5247bae5
Rikardo <rikardo@marenzzi.de>
2020-12-09 14:25:48 +0100

    app deployed

--- backend/src/main/java/de/neuefische/rikardo/dub/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/rikardo/dub/security/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/**").authenticated()
                 .antMatchers("/database/**").authenticated()
